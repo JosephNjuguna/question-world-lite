d378e04fd6e2de1f8d0b38d12145e5b3642e95ba
JosephNjuguna <josephnjuguna482@gmail.com>
2018-12-16 18:31:41 +0300

    [starts #162686786] user create account api(signup)

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask 
+from data import userdata
 
 app = Flask(__init__)
+user = userdata.Users.authdata()
+
+@app.route('/api/v1/auth/signup',methods=['POST'])
+def user_registration():
+	data = request.get_json()
+	if data == {}:
+		return "Empty Json"
+	for key in data:
+		if data[key] == '':
+			return "empty"
+	add_user = {}
+	add_user['name'] = data['name']
+	add_user['email'] = data['email']
+	add_user['password'] = data['password']
+	add_user['passwordconfirm'] = data['passwordconfirm']
+	add_user['admin'] = False
+	user.append(add_user)
+	return jsonify({"updated users": user})
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
