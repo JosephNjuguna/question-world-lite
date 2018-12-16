c9988eb8005c436bfd6871a069269b52edda3942
JosephNjuguna <josephnjuguna482@gmail.com>
2018-12-16 18:46:37 +0300

    [ft #162687349] set default data structures

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask 
+from flask import Flask
+from data import questionsdata
+from data import userdata
 
 app = Flask(__init__)
+quiz = questionsdata.Question.qdata()
+user = userdata.Users.authdata()
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
