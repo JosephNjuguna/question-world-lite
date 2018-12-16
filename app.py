e044f2bad38e33dda0d9126e92cb273cd0e9b288
JosephNjuguna <josephnjuguna482@gmail.com>
2018-12-16 20:22:09 +0300

    [start #162686791] create user post question api

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__init__)
 quiz = questionsdata.Question.qdata()
 user = userdata.Users.authdata()
 
+@app.route('/api/v1/questions', methods= ['POST'])
+def add_questions():
+	questiondata = request.get_json()
+	question_added = {}
+	question_added['question']= questiondata['question']
+	question_added['author']= questiondata['author']
+	question_added['id']=questiondata['id']
+	quiz.append(question_added)
+	return jsonify({"data":quiz})
 
 if __name__ == '__main__':
 	app.run(debug = True)
