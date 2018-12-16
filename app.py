2c195558ab9e2c52a3a3374ed9063ad2f3abdd98
JosephNjuguna <josephnjuguna482@gmail.com>
2018-12-16 20:45:26 +0300

    [starts #162686804] create user update answer

--- app.py
+++ app.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 app = Flask(__init__)
 
+def update_question(id):
+	id = int(id)
+	quiz_json = request.get_json()
+	single_quiz = [q for q in quiz if q['id'] == id]
+	if not single_quiz:
+		return "Invalid id"
+	single_quiz[0]['question'] = quiz_json['question']
+	return jsonify({"updated data":quiz})
+	
 if __name__ == '__main__':
 	app.run(debug = True)
