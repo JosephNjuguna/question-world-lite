46d0a72ee17573385b40af26d8892abc011a89e8
JosephNjuguna <josephnjuguna482@gmail.com>
2018-12-16 20:31:14 +0300

    [starts #162686794] user can view questions and answers api endpoint

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask 
+from data import questionsdata
 
 app = Flask(__init__)
+
+quiz = questionsdata.Question.qdata()
+
+@app.route('/api/v1/questions', methods= ['GET'])
+def get_all_questions():
+	if quiz =='':
+		return "no Data"
+	return jsonify({"data":quiz})
 
 if __name__ == '__main__':
 	app.run(debug = True)
