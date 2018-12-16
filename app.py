686f41e508a47fd92886431d141f40970f648747
JosephNjuguna <josephnjuguna482@gmail.com>
2018-12-16 20:40:50 +0300

    [starts #162686790] create user delete question api

--- app.py
+++ app.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 app = Flask(__init__)
 
+@app.route('/api/v1/questions/<string:id>',methods=['DELETE'])
+def delete_questions(id):
+	id = int(id)
+	single_quiz = [q for q in quiz if q['id'] == id]
+	if not single_quiz:
+		return "Id not found"
+	quiz.remove(single_quiz[0])
+	return jsonify({"deleted data": quiz})
+	
 if __name__ == '__main__':
 	app.run(debug = True)
