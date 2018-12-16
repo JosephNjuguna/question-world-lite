233f622cf957afb766f8de2d729109fbdd7e192a
JosephNjuguna <josephnjuguna482@gmail.com>
2018-12-16 18:41:33 +0300

    [starts #162686787] create user login api

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask 
+from data import userdata
 
 app = Flask(__init__)
+
+user = userdata.Users.authdata()
+
+@app.route('/api/v1/auth/login',methods=['POST', 'GET'])
+def user_log_in():
+	data = request.get_json()
+	if data == {}:
+		return "Empty Json"
+	user_name = data['name']
+	user_auth_password = data['password']
+	info_check = [info for info in user if info['name'] == user_name or info['password'] == user_auth_password]
+	if not info_check:
+		return "Check your Auth details"
+	return jsonify({"user": info_check})
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
